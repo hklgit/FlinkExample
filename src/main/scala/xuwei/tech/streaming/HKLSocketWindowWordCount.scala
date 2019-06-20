e41988b20374046570835c09aba61bf19e427e5f
hankunlun <811653654@qq.com>
2019-06-20 11:38:09 +0800

    stream-wordcount

---
+++ src/main/scala/xuwei/tech/streaming/HKLSocketWindowWordCount.scala
@@ -0,0 +1,85 @@
+package xuwei.tech.streaming
+
+import java.security.Policy.Parameters
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
+
+/**
+  * Created by Kerven on 2019/6/20.
+  *
+  * 　　　　　　　   ┏┓　   ┏┓+ +
+  * 　　　　　　　┏┛┻━━━┛┻┓ + +
+  * 　　　　　　　┃　　　　　　　┃
+  * 　　　　　　　┃　　　━　　　┃ ++ + + +
+  * 　　　　　　 ████━████ ┃+
+  * 　　　　　　　┃　　　　　　　┃ +
+  * 　　　　　　　┃　　　┻　　　┃
+  * 　　　　　　　┃　　　　　　　┃ + +
+  * 　　　　　　　┗━┓　　　┏━┛
+  * 　　　　　　　　　┃　　　┃
+  * 　　　　　　　　　┃　　　┃ + + + +
+  * 　　　　　　　　　┃　　　┃			
+  * 　　　　　　　　　┃　　　┃ +			
+  * 　　　　　　　　　┃　　　┃
+  * 　　　　　　　　　┃　　　┃　　+
+  * 　　　　　　　　　┃　 　　┗━━━┓ + +
+  * 　　　　　　　　　┃ 　　　　　　　┣┓
+  * 　　　　　　　　　┃ 　　　　　　　┏┛
+  * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+  * 　　　　　　　　　　┃┫┫　┃┫┫
+  * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+  *
+  * Don't bibi , show me the code
+  *
+  *
+  *入门程序 wordcount
+  *
+  * nc -l 9000
+  * 直接走起
+  *
+  */
+object HKLSocketWindowWordCount {
+
+  case class WordCount(word:String,count:Int)
+
+  def main(args: Array[String]): Unit = {
+
+    // first step get the env
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+
+//    ParameterTool.fromArgs(args).getInt("port")
+
+    // import this is important for implict transformation
+    import org.apache.flink.api.scala._
+
+   val ds = env.socketTextStream("192.168.0.159",9000)
+
+    val result = ds.flatMap(data => data.split("\\s"))
+      .map(word => WordCount(word, 1))
+      .keyBy("word")
+      .timeWindow(Time.seconds(5), Time.seconds(1))
+      .sum("count")
+
+    result.print().setParallelism(1)
+
+    env.execute("HKLSocketWindowWordCount")
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+}
